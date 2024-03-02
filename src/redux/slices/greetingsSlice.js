27d9a015b6dca0587dd73568540f5ad70febec69
Gumaro Monroy <maromvz@gmail.com>
2024-03-02 12:22:20 -0600

    Create slice for greetings data, and async reducer for fetching from API

---
+++ src/redux/slices/greetingsSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { GREETINGS_URL } from "../../database_urls/urls";
+import axios from "axios";
+
+const initialState = {
+  randomGreeting: ""
+};
+
+const fetchGreetings = createAsyncThunk("greetings/fetchGreetings", async (empty, { rejectWithValue }) => {
+  try {
+    const req = await axios.get(GREETINGS_URL);
+    const res = req.data;
+    return res;
+  } catch(error) {
+    return rejectWithValue(error)
+  }
+});
+
+const greetingsSlice = createSlice({
+  name: "greetings",
+  initialState,
+  reducers: {
+
+  },
+  extraReducers: (builder) => {
+    builder
+    .addCase(fetchGreetings.fulfilled, (state, { payload }) => {
+      state.randomGreeting = payload;
+    })
+    .addCase(fetchGreetings.rejected, (state, { payload }) => {
+      state.randomGreeting = payload;
+    });
+  }
+})
+
+export default greetingsSlice.reducer
+
+export { fetchGreetings }
